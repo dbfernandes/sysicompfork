8045c9f742abe935804ab76f07c8b4dc8daf12d0
Sergio Augusto <sergioaugusto.bezerra.jr@gmail.com>
2021-10-27 16:06:47 -0400

    [core] Muda padrao de importaçao para (import/export default) | Muda nome do layout padrao

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,20 @@
+/* Import routes */
 import express from 'express'
 import inicioRoutes from './inicio'
 import autenticacaoRoutes from './autenticacao'
 import usuariosRoutes from './usuarios'
 import projetosRoutes from './projetos'
 //const { isUsuarioAutenticado } = require('../utils/autenticacao-middleware')
+import inscricaoRouter from './inscricao';
 
 const router = express.Router()
 
+/* Add routes */
 //router.use('/', isUsuarioAutenticado)
 router.use('/', inicioRoutes)
 router.use('/', autenticacaoRoutes)
 router.use('/usuarios', usuariosRoutes)
 router.use('/projetos', projetosRoutes)
+router.use('/inscricao', inscricaoRouter);
 
-module.exports = router
+export default router
