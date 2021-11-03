f847614233aa525e4afcbbe3be0d9091a0015c09
maktheus <maktheus@gmail.com>
2021-11-03 11:59:43 -0400

    adicionar front

--- src/controllers/selecaoPpgi.js
+++ src/controllers/selecaoPpgi.js
@@ -1,9 +1,11 @@
 const adicionar = (req, res) => {
     if (req.method === "GET") {
-       return res.render('/', { 
+       return res.render('layouts/selecaoppgi/selecaoppgi-adicionar', { 
           nome: req.session.nome
       })
     } else {
        console.log("cadastrar no banco")
     }
  }
+
+ export default { adicionar}
