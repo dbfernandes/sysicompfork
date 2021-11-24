32e1e7fa5ace6108ad07b06ea21d1695048e97a8
Uchoa <maktheus@gmail.com>
2021-11-24 11:42:21 -0400

    migration update

--- src/database/migrations/20211124145722-create-selecao.js
+++ src/database/migrations/20211124145722-create-selecao.js
@@ -2,20 +2,63 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('Selecao', {
+      numero_Primaria: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      vaga_Doutorado: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+      cotas_Doutorado: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+      vaga_Mestrado: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+      cotas_Mestrado: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+      carta_Orientador:{
+        allowNull : false,
+        type: Sequelize.STRING
+      },
+      carta_Recomendacao:{
+        allowNull : false,
+        type : Sequelize.STRING
+      },
+      documento:{
+      allowNull : false,
+      type : Sequelize.STRING
+      },
+      curso:{
+        allowNull : false,
+        type : Sequelize.STRING
+      },
+      status:{
+        allowNull : false,
+        type : Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    
+    await queryInterface.dropTable('Selecao');
+    
   }
 };
