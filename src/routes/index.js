1a8b60bf780e81e00a71aa2bc12b43f6439530fa
Uchoa <maktheus@gmail.com>
2021-12-24 16:01:54 -0400

    organizacao de index.js routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,18 @@
 /* Import routes */
 import express from 'express'
-import inicioRoutes from './inicio.routes'
-import autenticacaoRoutes from './autenticacao.routes'
-import usuariosRoutes from './usuarios.routes'
-import projetosRoutes from './projetos.routes'
-import selecaoppgiRoutes from './selecaoppgi.routes'
+import inicioRoutes from './inicio'
+import autenticacaoRoutes from './autenticacao'
+import usuariosRoutes from './usuarios'
+import projetosRoutes from './projetos'
+import editalRouter from './edital.routes'
+import selecaoppgiRouter from './selecaoppgi';
 
 const router = express.Router()
 
 //const { isUsuarioAutenticado } = require('../utils/autenticacao-middleware')
-import selecaoppgiRouter from './selecaoppgi';
-
 
 /* Add routes */
 //router.use('/', isUsuarioAutenticado)
-
 // router.use('/', autenticacaoRoutes)
 //router.use('/inscricao', inscricaoRouter);
 
@@ -25,7 +23,7 @@ router.use('/', autenticacaoRoutes)
 router.use('/usuarios', usuariosRoutes)
 router.use('/projetos', projetosRoutes)
 router.use('/selecaoppgi', selecaoppgiRouter);
-router.use('/edital', selecaoppgiRoutes)
+router.use('/edital', editalRouter)
 
 
 export default router
