546ef0fe5966b10f3364a492909047a8988dca7c
Luan <luanrb19@gmail.com>
2021-12-28 14:37:28 -0400

    Modificacoes no model

--- src/models/index.js
+++ src/models/index.js
@@ -8,8 +8,8 @@ const env = process.env.NODE_ENV || 'development';
 const config = require("../config/database-sample")[env];
 const db = {};
 
-
 let sequelize;
+
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
