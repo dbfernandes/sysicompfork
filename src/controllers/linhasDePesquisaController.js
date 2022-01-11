612c3901e7886505ac658198be1e60ddd3aaad95
Luan <luanrb19@gmail.com>
2022-01-10 21:46:39 -0400

    middleware de busca

--- src/controllers/linhasDePesquisaController.js
+++ src/controllers/linhasDePesquisaController.js
@@ -1,72 +1,34 @@
 import linhasDePesquisaService from '../services/linhasDePesquisaService';
 
 const locals = {
-  layout: "main",
+  layout: 'dataTable',
 };
 
-const lines = [
-  {
-    id: 1,
-    nome: 'Banco de Dados e Recuperacao de Informacao',
-    sigla: 'BD e RI',
-    cor: 'rgb(1,68,248)',
-  },
-  {
-    id: 2,
-    nome: 'Sistemas Embarcados e Engenharia de Software',
-    sigla: 'SE&ES',
-    cor: 'rgb(211,236,40)',
-  },
-  {
-    id: 3,
-    nome: 'Inteligencia Artificial',
-    sigla: 'IA',
-    cor: 'rgb(254,216,244)',
-  },
-  {
-    id: 4,
-    nome: 'Visao Computacional e Robotica',
-    sigla: 'Visao',
-    cor: 'rgb(202,56,176)',
-  },
-  {
-    id: 5,
-    nome: 'Redes e Telecomunicacoes',
-    sigla: 'Redes',
-    cor: 'rgb(69,94,35)',
-  },
-  {
-    id: 6,
-    nome: 'Otimizacao, Alg. e Complexidade Computacional',
-    sigla: 'Otim.',
-    cor: 'rgb(48,173,165)',
-  },
-  {
-    id: 7,
-    nome: 'Software, Interacao e Aplicacoes',
-    sigla: 'SIA',
-    cor: 'rgb(56,205,28)',
-  },
-];
+const pageTitle = 'Linhas De Pesquisa';
 
-const listar = async (req, res) => {
-  switch (req.method) {
-    case "GET": {
-      // const linhaDePesquisa = await linhasDePesquisaService.list();
+const listar = async (_req, res) => {
+  const linhaDePesquisa = await linhasDePesquisaService.list();
+  const doesNotExists = !linhaDePesquisa || linhaDePesquisa.length === 0;
 
-      // if (!linhaDePesquisa) res.status(400).json({ message: 'Nenhuma linha de pesquisa cadastrada' });
+  if (doesNotExists) res.status(400).json({ message: 'Nenhuma linha de pesquisa cadastrada' });
 
-      return res
-        .status(200)
-        .render("linhasDePesquisa/linhasDePesquisa-listar", { linhaDePesquisa: lines, ...locals });
-    }
-
-    case "POST":
-      return res.json({ message: "cant post here" });
-
-    default:
-      return res.json("404");
-  }
+  return res
+    .status(200)
+    .render('linhasDePesquisa/linhasDePesquisa-listar', { linhaDePesquisa, pageTitle, ...locals });
 };
 
-export default { listar };
+const buscar = async (req, res) => {
+  const { id } = req.params;
+
+  const result = await linhasDePesquisaService.findById(id);
+
+  if (!result) return res.status(400).json({ message: 'Linha de Pesquisa Não Encontrada!'});
+
+  const { nome, sigla, icone, cor } = result;
+
+  return res
+    .status(200)
+    .render('linhasDePesquisa/linhasDePesquisa-busca', { nome, sigla, icone, cor, pageTitle, ...locals });
+};
+
+export default { listar, buscar };
