56ca8aef5b1c9738597725e4c87ead045baa4ef3
DaniSanT17 <dan-santiago@live.com>
2023-12-01 23:22:41 -0400

    Add avatar lattes upload

--- src/routes/curriculo.routes.js
+++ src/routes/curriculo.routes.js
@@ -1,8 +1,23 @@
 import express from 'express'
+import multer from 'multer'
 import curriculoController from '../controllers/curriculoController'
+
+
+const timestamp = new Date().getTime();
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback)=>{
+        callback(null, "/app/src/uploads");
+    },
+    filename: (req, file, callback)=>{
+        callback(null, `${timestamp}-`+file.originalname);
+    }
+})
+
+const uploads = multer({storage: storage})
 const router = express.Router()
 
 router.get('/', curriculoController.visualizar)
-router.post('/upload', curriculoController.carregar)
+router.post('/upload', uploads.single("file"), curriculoController.carregar)
 
 export default router
