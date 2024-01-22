821612efa175a8e4d035537670a877b430a8b487
Alexandre da Fonseca Vieira <107149857+SnipAlex@users.noreply.github.com>
2024-01-22 13:11:33 -0400

    Modificações no estilo das linhas.

--- src/services/editalGerarPlanilha.js
+++ src/services/editalGerarPlanilha.js
@@ -5,55 +5,95 @@ const exceljs = require('exceljs');
 // Pegar o edital do banco de dados
 
 // Transformar os dados para JSON
-let testeDados = {nome: "teste", cpf: "12345678910", rg: "123456789", dataNascimento: "01/01/2000", sexo: "Masculino", tipo: "mestrado"}
-let testeDados2 = {nome: "teste2", cpf: "12345678910", rg: "123456789", dataNascimento: "01/01/2000", sexo: "Masculino", tipo: "doutorado"}  
+let testeDados = { nome: 'Teste', email: 'asd@email.com', 
+    inscricao: '123456', linha: 'Teste', orientador: 'Teste', bolsa: 'Teste', nivel: 'Teste' };
+
+let testeDados2 = { nome: 'Teste2', email: 'asd@asd.com',
+    inscricao: '123456', linha: 'Teste', orientador: 'Teste', bolsa: 'Teste', nivel: 'Teste' };
+
 let dados = JSON.stringify(testeDados);
 let dados2 = JSON.stringify(testeDados2);
 
 async function gerarPlanilha(editaId) {
     // Create a new instance of a Workbook class
     let workbook = new exceljs.Workbook();
-    let worksheet = workbook.addWorksheet('Planilha de Candidatos');
-    
+    // Novas abas na planilhas
+    let worksheetCandidatos = workbook.addWorksheet('Candidato');
+    let worksheetProvas = workbook.addWorksheet('Provas');
+    let worksheetPropostas = workbook.addWorksheet('Propostas');
     
 
-    worksheet.columns = [
-        { header: 'Nome', key: 'nome', width: 30 },
-        { header: 'CPF', key: 'cpf', width: 15 },
-        { header: 'RG', key: 'rg', width: 15 },
-        { header: 'Data de Nascimento', key: 'dataNascimento', width: 20 },
-        { header: 'Sexo', key: 'sexo', width: 10 },
-        { header: 'Tipo', key: 'tipo', width: 10 },
+    worksheetCandidatos.columns = [
+        { header: 'Nome', key: 'nome', width: 40 },
+        { header: 'Email', key: 'email', width: 40 },
+        { header: 'Inscrição', key: 'inscricao', width: 20 },
+        { header: 'Linha', key: 'linha', width: 10 },
+        { header: 'Orientador', 'key': 'orientador', width: 40 },
+        { header: 'Bolsa', key: 'bolsa', width: 15 },
+        { header: 'Nível', key: 'nivel', width: 15 },
     ];
+
+    let headerRow = worksheetCandidatos.getRow(1);
+    headerRow.eachCell((cell, number) => {
+        cell.font = { bold: true, color: { argb: '00000' } };
+        cell.alignment = { vertical: 'middle', horizontal: 'center' };
+    });
+    headerRow.height = 30;
+    
     // Cria um separador
-    worksheet.spliceRows(1,0, [])
-    let separador = worksheet.getCell(1,1);
-    separador.value = 'Candidatos';
-    separador.fill = {
-        type: 'pattern',
-        pattern: 'solid',
-        fgColor: { argb: 'FFC0C0C0' },
-        bgColor: { argb: 'FFC0C0C0' }
-    };
-    separador.alignment = { vertical: 'middle', horizontal: 'center' };
+    worksheetCandidatos.spliceRows(1,0, [])
+
+    for (let i = 1; i <= 7; i++) {
+        let cell = worksheetCandidatos.getCell(1, i); 
+        cell.fill = {
+            type: 'pattern',
+            pattern: 'solid',
+            fgColor: { argb: 'FFC0C0C0' },
+            bgColor: { argb: 'FFC0C0C0' }
+        };
+        cell.font = {color: { argb: 'FFFFFF' } };
+
+        if (i == 4) {
+            cell.value = 'Mestrados';
+            cell.alignment = { vertical: 'middle', horizontal: 'center' };
+        } 
+    }
 
     // Dados testes
-    worksheet.addRow(JSON.parse(dados));
+    worksheetCandidatos.addRow(JSON.parse(dados));
+    
+    // Cria um separador
+    let separador2 = worksheetCandidatos.addRow(['']);
+    for (let i = 1; i <= 7; i++) {
+        let cell = worksheetCandidatos.getCell(separador2.number, i); 
+        cell.fill = {
+            type: 'pattern',
+            pattern: 'solid',
+            fgColor: { argb: 'FFC0C0C0' },
+            bgColor: { argb: 'FFC0C0C0' }
+        };
+        cell.font = {color: { argb: 'FFFFFF' } };
 
-    let separador2 = worksheet.addRow(['']);
-    separador2.getCell(1).value = 'Professores';
-    separador2.getCell(1).fill = {
-        type: 'pattern',
-        pattern: 'solid',
-        fgColor: { argb: 'FFC0C0C0' },
-        bgColor: { argb: 'FFC0C0C0' }
-    };
-    separador2.alignment = { vertical: 'middle', horizontal: 'center' };
+        if (i == 4) {
+            cell.value = 'Doutorados';
+            cell.alignment = { vertical: 'middle', horizontal: 'center' };
+        } 
+    }
 
-    worksheet.addRow(['Nome', 'CPF', 'RG', 'Data de Nascimento', 'Sexo', 'Tipo']);
+    let linhaHeaderDoutorados = worksheetCandidatos.addRow(
+        ['Nome', 'Email', 'Inscrição', 'Linha', 'Orientador', 'Bolsa', 'Nível']
+    );
+
+    linhaHeaderDoutorados.eachCell((cell, number) => {
+        cell.font = { bold: true, color: { argb: '00000' } };
+        cell.alignment = { vertical: 'middle', horizontal: 'center' };
+    });
+    linhaHeaderDoutorados.height = 30;
 
     // Dados testes
-    worksheet.addRow(JSON.parse(dados2));
+    worksheetCandidatos.addRow(JSON.parse(dados2));
+
+
 
     // Save Excel on Hard Disk
     await workbook.xlsx.writeFile('planilha.xlsx')
