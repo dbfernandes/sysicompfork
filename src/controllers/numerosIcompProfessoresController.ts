71a97c0f75ec4be01f2b41fee2e4fc11c8a10d10
marcelo leda <marcelo.leda@clicksoft.com.br>
2024-05-14 10:02:54 -0400

    repair: alterando para typescript

---
+++ src/controllers/numerosIcompProfessoresController.ts
@@ -0,0 +1,258 @@
+import { Request, Response } from 'express';
+import UsuarioService from '../services/usuarioService';
+import DocenteService from '../services/docenteService';
+
+// Escolha do Layout
+const layoutMain = {
+  layout: 'numerosIcompMain'
+};
+
+const layoutDashboard = {
+  layout: 'numerosIcompDashboard'
+};
+
+// Listagem de Docentes
+
+const professores = async (req: Request, res: Response) => {
+  switch (req.method) {
+    case 'GET':
+      try {
+        const { lng } = req.query;
+        const professores = await UsuarioService.listarTodosPorCondicao({
+          professor: 1
+        });
+        return res.status(200).render('numerosIcomp/docentes', {
+          lng,
+          professores,
+          ...layoutMain
+        });
+      } catch (error) {
+        return res.status(502).send('O Servidor não obteve uma resposta válida. Bad Gateway (502)');
+      }
+    default:
+      return res.status(400).send('A requisição enviada ao servidor é invalida. Bad Request (400)');
+  }
+};
+
+// Perfil
+
+const perfil = async (req: Request, res: Response) => {
+  switch (req.method) {
+    case 'GET':
+      try {
+        const { lng } = req.query;
+        const { id } = req.params;
+        const professor = await DocenteService.listarPerfil(id);
+        if (!professor) {
+          return res.redirect('/numerosIcomp/docentes?lng=' + lng);
+        }
+        return res.render('numerosIcomp/perfil/perfil', {
+          lng,
+          professor,
+          ...layoutDashboard
+        });
+      } catch (error) {
+        return res.status(502).send('O Servidor não obteve uma resposta válida. Bad Gateway (502)');
+      }
+    default:
+      return res.status(400).send('A requisição enviada ao servidor é invalida. Bad Request (400)');
+  }
+};
+
+const publicacoes = async (req: Request, res: Response) => {
+  switch (req.method) {
+    case 'GET':
+      try {
+        const { lng } = req.query;
+        const { id } = req.params;
+        const professor = await DocenteService.listarPerfil(id);
+        if (!professor) {
+          return res.redirect('/numerosIcomp/docentes?lng=' + lng);
+        }
+        const publicacoes = await DocenteService.listarPublicacoes(id);
+
+        const currentYear = new Date().getFullYear();
+        const anos = [...Array(11).keys()].map(i => i + currentYear - 10);
+        const graficoArtigosConferencias = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        const graficoArtigosPeriodicos = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        publicacoes.artigosConferencias.forEach(artigo => {
+          const idx = anos.findIndex(e => e === artigo.ano);
+          if (idx === -1) {
+            graficoArtigosConferencias[0] = graficoArtigosConferencias[0] + 1;
+          } else {
+            graficoArtigosConferencias[idx] = graficoArtigosConferencias[idx] + 1;
+          }
+        });
+
+        publicacoes.artigosPeriodicos.forEach(artigo => {
+          const idx = anos.findIndex(e => e === artigo.ano);
+          if (idx === -1) {
+            graficoArtigosPeriodicos[0] = graficoArtigosPeriodicos[0] + 1;
+          } else {
+            graficoArtigosPeriodicos[idx] = graficoArtigosPeriodicos[idx] + 1;
+          }
+        });
+
+        return res.render('numerosIcomp/perfil/perfil-publicacao', {
+          lng,
+          professor,
+          publicacoes,
+          paperConfLen: publicacoes.artigosConferencias.length,
+          paperPerLen: publicacoes.artigosPeriodicos.length,
+          bookLen: publicacoes.livros.length,
+          chapterLen: publicacoes.capitulos.length,
+          ...layoutDashboard,
+          anos,
+          graficoArtigosConferencias,
+          graficoArtigosPeriodicos
+        });
+      } catch (error) {
+        return res.status(502).send('O Servidor não obteve uma resposta válida. Bad Gateway (502)');
+      }
+    default:
+      return res.status(400).send('A requisição enviada ao servidor é invalida. Bad Request (400)');
+  }
+};
+
+const pesquisa = async (req: Request, res: Response) => {
+  switch (req.method) {
+    case 'GET':
+      try {
+        const { lng } = req.query;
+        const { id } = req.params;
+        const professor = await DocenteService.listarPerfil(id);
+        if (!professor) {
+          return res.redirect('/numerosIcomp/docentes?lng=' + lng);
+        }
+        const projetos = await DocenteService.listarPesquisas(id);
+
+        const currentYear = new Date().getFullYear();
+        const anos = [...Array(10).keys()].map(i => i + currentYear - 9);
+
+        const graficoProjetos = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+        projetos.forEach(projeto => {
+          const anosProjeto = projeto.fim === 0
+            ? [...Array(currentYear - projeto.inicio).keys()].map(i => i + currentYear - (currentYear - projeto.inicio - 1))
+            : [...Array(projeto.fim - projeto.inicio).keys()].map(i => i + projeto.fim - (projeto.fim - projeto.inicio - 1));
+          anosProjeto.forEach(ano => {
+            const idx = anos.findIndex(e => e === ano);
+            if (idx > -1) {
+              graficoProjetos[idx] = graficoProjetos[idx] + 1;
+            }
+          });
+        });
+        return res.render('numerosIcomp/perfil/perfil-projeto', {
+          lng,
+          professor,
+          projetos,
+          projetosLen: projetos.length,
+          ...layoutDashboard,
+          anos,
+          graficoProjetos
+        });
+      } catch (error) {
+        return res.status(502).send('O Servidor não obteve uma resposta válida. Bad Gateway (502)');
+      }
+
+    default:
+      return res.status(400).send('A requisição enviada ao servidor é invalida. Bad Request (400)');
+  }
+};
+
+const orientacao = async (req: Request, res: Response) => {
+  switch (req.method) {
+    case 'GET':
+      try {
+        const { lng } = req.query;
+        const { id, tipo } = req.params;
+        const tipos = ['graduacao', 'mestrado', 'doutorado'];
+        const t = tipos.findIndex(e => e === tipo) + 1;
+        if (t === 0) {
+          return res.redirect('/numerosIcomp/docentes?lng=' + lng);
+        }
+        const professor = await DocenteService.listarPerfil(id);
+        if (!professor) {
+          return res.redirect('/numerosIcomp/docentes?lng=' + lng);
+        }
+        const orientacoes = await DocenteService.listarOrientacoes(id, t);
+
+        const currentYear = new Date().getFullYear();
+        const anos = [...Array(10).keys()].map(i => i + currentYear - 9);
+
+        const graficoOrientacoesConcluidas = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        const graficoOrientacoesAndamento = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+        orientacoes.concluidas.forEach(orientacao => {
+          const idx = anos.findIndex(e => e === orientacao.ano);
+          if (idx === -1) {
+            graficoOrientacoesConcluidas[0] = graficoOrientacoesConcluidas[0] + 1;
+          } else {
+            graficoOrientacoesConcluidas[idx] = graficoOrientacoesConcluidas[idx] + 1;
+          }
+        });
+
+        orientacoes.andamento.forEach(orientacao => {
+          const idx = anos.findIndex(e => e === orientacao.ano);
+          if (idx === -1) {
+            graficoOrientacoesAndamento[0] = graficoOrientacoesAndamento[0] + 1;
+          } else {
+            graficoOrientacoesAndamento[idx] = graficoOrientacoesAndamento[idx] + 1;
+          }
+        });
+
+        return res.render('numerosIcomp/perfil/perfil-orientacao', {
+          lng,
+          professor,
+          orientacoes,
+          orientacoesConcluidasLen: orientacoes.concluidas.length,
+          orientacoesAndamentoLen: orientacoes.andamento.length,
+          ...layoutDashboard,
+          anos,
+          tipo: tipo === 'graduacao' ? 'Graduação' : tipo.charAt(0).toUpperCase() + tipo.slice(1),
+          graficoOrientacoesAndamento,
+          graficoOrientacoesConcluidas
+        });
+      } catch (error) {
+        return res.status(502).send('O Servidor não obteve uma resposta válida. Bad Gateway (502)');
+      }
+    default:
+      return res.status(400).send('A requisição enviada ao servidor é invalida. Bad Request (400)');
+  }
+};
+
+const premios = async (req: Request, res: Response) => {
+  switch (req.method) {
+    case 'GET':
+      try {
+        const { lng } = req.query;
+        const { id } = req.params;
+        const professor = await DocenteService.listarPerfil(id);
+        if (!professor) {
+          return res.redirect('/numerosIcomp/docentes?lng=' + lng);
+        }
+        const premios = await DocenteService.listarPremios(id);
+
+        return res.render('numerosIcomp/perfil/perfil-premio', {
+          lng,
+          premios,
+          professor,
+          premiosLen: premios.length,
+          ...layoutDashboard
+        });
+      } catch (error) {
+        return res.status(502).send('O Servidor não obteve uma resposta válida. Bad Gateway (502)');
+      }
+    default:
+      return res.status(400).send('A requisição enviada ao servidor é invalida. Bad Request (400)');
+  }
+};
+
+export default {
+  professores,
+  perfil,
+  publicacoes,
+  pesquisa,
+  orientacao,
+  premios
+};
---
+++ src/services/alunoService.ts
@@ -0,0 +1,99 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+class AlunoService {
+  async adicionarVarios(alunos: any[]): Promise<void> {
+    if (alunos !== undefined && alunos.length > 0) {
+      await prisma.aluno.deleteMany(); // Remove todos os registros existentes
+      await prisma.aluno.createMany({ data: alunos }); // Cria os novos registros
+    }
+  }
+
+  async listarTodos(curso: string, formado: any): Promise<any[]> {
+    return prisma.aluno.findMany({
+      where: {
+        curso,
+        formado
+      }
+    });
+  }
+
+  async contarTodos(): Promise<any> {
+    const contagem = {
+      matriculados: {
+        CC: 0,
+        SI: 0,
+        ES: 0,
+        D: 0,
+        M: 0
+      },
+      egressos: {
+        PD: 254,
+        CC: 0,
+        SI: 0,
+        ES: 0,
+        D: 0,
+        M: 0
+      },
+      data: new Date()
+    };
+
+    const alunos = await prisma.aluno.findMany({
+      where: {
+        curso: {
+          not: 'Processamento de Dados'
+        }
+      },
+      select: {
+        formado: true,
+        curso: true,
+        createdAt: true
+      }
+    });
+
+    if (alunos.length > 0) {
+      contagem.data = alunos[0].createdAt;
+      alunos.forEach(aluno => {
+        if (!aluno.formado) {
+          switch (aluno.curso) {
+            case 'Ciência da Computação':
+              contagem.matriculados.CC += 1;
+              break;
+            case 'Engenharia de Software':
+              contagem.matriculados.ES += 1;
+              break;
+            case 'Mestrado':
+              contagem.matriculados.M += 1;
+              break;
+            default:
+              contagem.matriculados.D += 1;
+              break;
+          }
+        } else {
+          switch (aluno.curso) {
+            case 'Ciência da Computação':
+              contagem.egressos.CC += 1;
+              break;
+            case 'Engenharia de Software':
+              contagem.egressos.ES += 1;
+              break;
+            case 'Sistemas de Informação':
+              contagem.egressos.SI += 1;
+              break;
+            case 'Mestrado':
+              contagem.egressos.M += 1;
+              break;
+            default:
+              contagem.egressos.D += 1;
+              break;
+          }
+        }
+      });
+    }
+
+    return contagem;
+  }
+}
+
+export default new AlunoService();
