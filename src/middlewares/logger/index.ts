6a58e606821f77c3e81c5b7043da69a04f2614d0
Alexandre da Fonseca Vieira <107149857+SnipAlex@users.noreply.github.com>
2024-06-19 17:26:38 -0400

    refactor: nova formatação de mensagens

--- src/middlewares/logger/index.ts
+++ src/middlewares/logger/index.ts
@@ -10,17 +10,17 @@ import { Request, Response, NextFunction } from 'express'
 const logDir = path.join(__dirname, '../../../logs')
 
 const logger = winston.createLogger({
-  level: 'info',
   format: winston.format.combine(
     winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     winston.format.json()
   ),
+  // level: 'info',
   transports: [new winston.transports.File({ filename: path.join(logDir, 'info.log') })]
 })
 
 export function resgistro(req:Request, res: Response, next: NextFunction) {
   if (req.session.uid) {
-    logger.info(`SESSION ID: ${req.session.id}, USER ID:${req.session.uid}, URL: ${req.originalUrl}`)
+    logger.info(`USER ID:${req.session.uid}, URL: ${req.originalUrl}, SESSION ID: ${req.session.id}`)
   }
   next()
 }
