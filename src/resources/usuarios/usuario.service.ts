0b92d2bfe72dd669bcc13d08298b1298e899fba3
Alexandre da Fonseca Vieira <107149857+SnipAlex@users.noreply.github.com>
2025-01-30 19:58:34 -0400

    refactor: adicionado try-catch para algumas funções no service usuario

--- src/resources/usuarios/usuario.service.ts
+++ src/resources/usuarios/usuario.service.ts
@@ -1,6 +1,7 @@
 import crypto from 'crypto';
-import { Prisma, PrismaClient, Usuario } from '@prisma/client';
+import prisma from '../../client';
 import bcrypt from 'bcrypt';
+import { Usuario, Prisma } from '@prisma/client';
 import { generateHashPassword } from '../../utils/utils';
 import {
   CreateUsuarioDto,
@@ -8,64 +9,70 @@ import {
   UpdateUsuarioWithPassword,
   UsuarioWithDate,
 } from './usuario.types';
-const prisma = new PrismaClient();
 
 class UsuarioService {
   async adicionar(usuario: CreateUsuarioDto): Promise<Usuario> {
     const salt = await bcrypt.genSalt(12);
     const senhaHash = await bcrypt.hash(usuario.senhaHash, salt);
-
-    if (usuario.diretor === 1) {
-      const usuarioDiretor = await prisma.usuario.findFirst({
-        where: {
-          diretor: 1,
-        },
-      });
-
-      if (usuarioDiretor) {
-        // removido a verificação do id
-        await prisma.usuario.update({
+    try {
+      if (usuario.diretor === 1) {
+        const usuarioDiretor = await prisma.usuario.findFirst({
           where: {
-            id: usuarioDiretor.id,
-          },
-          data: {
-            diretor: 0,
+            diretor: 1,
           },
         });
+
+        if (usuarioDiretor) {
+          await prisma.usuario.update({
+            where: {
+              id: usuarioDiretor.id,
+            },
+            data: {
+              diretor: 0,
+            },
+          });
+        }
       }
+
+      return await prisma.usuario.create({ data: { ...usuario, senhaHash } });
+    } catch (error) {
+      throw new Error('Erro ao adicionar usuário');
     }
 
-    return await prisma.usuario.create({ data: { ...usuario, senhaHash } });
   }
 
   async alterar(id: number, user: UpdateUsuarioWithPassword): Promise<void> {
-    if ('senha' in user && user.senha !== '') {
-      user.senhaHash = await generateHashPassword(user.senha);
-    }
+    try {
+      if ('senha' in user && user.senha !== '') {
+        user.senhaHash = await generateHashPassword(user.senha);
+      }
 
-    if (user.diretor === 1) {
-      const usuarioAtual = await prisma.usuario.findFirst({
-        where: {
-          diretor: 1,
-        },
-      });
-
-      if (usuarioAtual && usuarioAtual.id !== id) {
-        await prisma.usuario.update({
+      if (user.diretor === 1) {
+        const usuarioAtual = await prisma.usuario.findFirst({
           where: {
-            id: usuarioAtual.id,
-          },
-          data: {
-            diretor: 0,
+            diretor: 1,
           },
         });
-      }
-    }
 
-    await prisma.usuario.update({
-      where: { id },
-      data: user,
-    });
+        if (usuarioAtual && usuarioAtual.id !== id) {
+          await prisma.usuario.update({
+            where: {
+              id: usuarioAtual.id,
+            },
+            data: {
+              diretor: 0,
+            },
+          });
+        }
+      }
+
+      await prisma.usuario.update({
+        where: { id },
+        data: user,
+      });
+    } catch (error) {
+      throw new Error('Erro ao alterar usuário');
+    }
   }
 
   async alterarInfo(id: number, user: UpdateUsuarioDto): Promise<void> {
@@ -177,13 +184,17 @@ class UsuarioService {
   }
 
   async recuperarSenha(token: string, data: Date, id: number): Promise<void> {
-    await prisma.usuario.update({
-      where: { id },
-      data: {
-        tokenResetSenha: token,
-        validadeTokenResetSenha: data,
-      },
-    });
+    try {
+      await prisma.usuario.update({
+        where: { id },
+        data: {
+          tokenResetSenha: token,
+          validadeTokenResetSenha: data,
+        },
+      });
+    } catch (error) {
+      throw new Error('Erro ao recuperar senha');
+    }
   }
 
   async atualizarTokenSenha(id: number) {
@@ -191,43 +202,49 @@ class UsuarioService {
     const timeAdd = process.env.TIME_MILLIS_EXPIRE_EMAIL || 3600000;
     const timeExpires = new Date();
     timeExpires.setTime(timeExpires.getTime() + Number(timeAdd));
-
-    await prisma.usuario.update({
-      where: {
-        id,
-      },
-      data: {
-        tokenResetSenha: token,
-        validadeTokenResetSenha: timeExpires,
-      },
-    });
-    return token;
+    try {
+      await prisma.usuario.update({
+        where: {
+          id,
+        },
+        data: {
+          tokenResetSenha: token,
+          validadeTokenResetSenha: timeExpires,
+        },
+      });
+    } catch (error) {
+      throw new Error('Erro ao atualizar token');
+    }
   }
 
   async mudarSenhaComToken({ token, password }) {
-    const usuario = await prisma.usuario.findFirst({
-      where: {
-        tokenResetSenha: token,
-      },
-    });
-    if (!usuario) {
-      throw new Error('Token inválido');
-    }
-    if (usuario.validadeTokenResetSenha < new Date()) {
-      throw new Error('Token expirado');
-    }
+    try {
+      const usuario = await prisma.usuario.findFirst({
+        where: {
+          tokenResetSenha: token,
+        },
+      });
+      if (!usuario) {
+        throw new Error('Token inválido');
+      }
+      if (usuario.validadeTokenResetSenha < new Date()) {
+        throw new Error('Token expirado');
+      }
 
-    const passwordHash = await generateHashPassword(password);
-    return await prisma.usuario.update({
-      where: {
-        id: usuario.id,
-      },
-      data: {
-        senhaHash: passwordHash,
-        tokenResetSenha: null,
-        validadeTokenResetSenha: null,
-      },
-    });
+      const passwordHash = await generateHashPassword(password);
+      return await prisma.usuario.update({
+        where: {
+          id: usuario.id,
+        },
+        data: {
+          senhaHash: passwordHash,
+          tokenResetSenha: null,
+          validadeTokenResetSenha: null,
+        },
+      });
+    } catch (error) {
+      throw new Error('Erro ao atualizar senha');
+    }
   }
 }
 
