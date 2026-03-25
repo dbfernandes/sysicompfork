3d05c975d9b1db29edc92e1d9e4705a4907ba8b9
Francisco Junior <rivailjunior0906@gmail.com>
2026-03-25 15:43:29 -0400

    ajuste migrations

--- prisma/migrations/20260225173955_add_titulacao/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `tipoFormacao` to the `LattesFormacaoAcademicaTitulacao` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `LattesFormacaoAcademicaTitulacao` ADD COLUMN `tipoFormacao` ENUM('GRADUACAO', 'ESPECIALIZACAO', 'MESTRADO', 'MESTRADO_PROFISSIONALIZANTE', 'DOUTORADO', 'POS_DOUTORADO', 'LIVRE_DOCENCIA', 'RESIDENCIA_MEDICA', 'CURSO_TECNICO_PROFISSIONALIZANTE', 'APERFEICOAMENTO', 'ENSINO_MEDIO', 'ENSINO_FUNDAMENTAL') NOT NULL;
